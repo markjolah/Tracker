db279fb4ad656e3112144a57f2efd55c893589c2
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-16 14:18:04 -0700

    updates for cross-compiling options

--- build.gcc4_9.debug.sh
+++ build.gcc4_9.debug.sh
@@ -7,7 +7,7 @@
 
 ARCH=gcc4_9
 FULL_ARCH=x86_64-${ARCH}-linux-gnu
-TOOLCHAIN_FILE=./cmake/Toolchains/Toolchain-${FULL_ARCH}.cmake
+TOOLCHAIN_FILE=./cmake/UncommonCMakeModules/Toolchains/Toolchain-${FULL_ARCH}.cmake
 INSTALL_PATH=_${ARCH}.install
 BUILD_PATH=_${ARCH}.build/Debug
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
@@ -17,9 +17,9 @@ ARGS="${ARGS} -DOPT_FIXUP_DEPENDENCIES=On"
 ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
 ARGS="${ARGS} -DOPT_FIXUP_BUILD_TREE_DEPENDENCIES=On"
 ARGS="${ARGS} -DOPT_INSTALL_GCC_DEPENDENCIES=On"
-rm -rf $INSTALL_PATH $BUILD_PATH
 
-set -e
+set -ex
+rm -rf $INSTALL_PATH $BUILD_PATH
 
 cmake -H. -B$BUILD_PATH -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -DCMAKE_INSTALL_PREFIX="$INSTALL_PATH" -DCMAKE_BUILD_TYPE=Debug ${ARGS}
 VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
