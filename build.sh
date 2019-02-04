5425ca342158727d2a2bfc4c6f439425d18eb0a5
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-04 16:44:29 -0700

    Build scripts update

--- build.sh
+++ build.sh
@@ -1,14 +1,32 @@
-##!/bin/bash
+#!/bin/bash
+# build.sh
+#
+# Release and Debug build to local install prefix with build-tree export
+#
+# For safety only delete the _install if and only if INSTALL_PATH hasn't been modified.
 
 INSTALL_PATH=_install
 BUILD_PATH=_build
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DBUILD_TESTING=ON -DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON"
-rm -rf $INSTALL_PATH $BUILD_PATH
 
-set -e
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
+ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
+ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
+ARGS="${ARGS} -DOPT_MATLAB=Off"
 
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS}
-cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${COMMON_ARGS}
+set -ex
+
+if [ "$INSTALL_PATH" == "_install" ]; then
+    rm -rf _install
+fi
+rm -rf $BUILD_PATH/Debug
+rm -rf $BUILD_PATH/Release
+cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${ARGS}
 cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
+cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${ARGS}
 cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
+
+
