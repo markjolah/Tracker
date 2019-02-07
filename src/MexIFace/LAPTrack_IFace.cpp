8ddddb2abbdec4ffd3babfbe481e6a3431da7d2f
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-07 16:01:59 -0700

    Build updates

--- src/MexIFace/LAPTrack_IFace.cpp
+++ src/MexIFace/LAPTrack_IFace.cpp
@@ -1,11 +1,11 @@
 /** @file LAPTrack_Iface.cpp
  *  @author Mark J. Olah (mjo at cs.unm.edu)
- *  @date 2015-2018
+ *  @date 2015-2019
  *  @brief The entry point for LAPTrack_Iface mex module.
  * 
  */
-#include "Tracker_IFace.h"
 #include "Tracker/LAPTrack.h"
+#include "Tracker_IFace.h"
 
 Tracker_IFace<tracker::LAPTrack> iface; /**< Global iface object provides a iface.mexFunction */
 
