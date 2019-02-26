5cc4e82392227c68940c2b2162dab263790eca4c
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-26 09:52:22 -0700

    New Build system

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Tracker
-Particle Tracking Trajectgory Connector
+Particle tracking trajectory connector using a sparse LAP solver.
+
+![Tracker problem](doc/images/tracker_problem.png)
+
+## Documentation
+The Tracker Doxygen documentation can be build with the `OPT_DOC` CMake option and is also available on online:
+  * [Tracker HTML Manual](https://markjolah.github.io/Tracker/index.html)
+  * [Tracker PDF Manual](https://markjolah.github.io/Tracker/pdf/Tracker-0.1-reference.pdf)
+  * [Tracker github repository](https://github.com/markjolah/Tracker)
---
+++ scripts/doc-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# scripts/doc-build.sh <cmake args ...>
+# Build documentation into the build tree
+# Works with Travis CI.
+
+NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRC_PATH=${SCRIPT_DIR}/..
+BUILD_PATH=${SCRIPT_DIR}/../_build/documentation
+INSTALL_PATH=${SCRIPT_DIR}/../_install.documentation
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DOPT_DOC=On"
+ARGS="${ARGS} -DBUILD_TESTING=Off"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=Off"
+
+set -ex
+#rm -rf $BUILD_PATH
+
+cmake -H${SRC_PATH} -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Release -Wdev $ARGS $@
+VERBOSE=1 cmake --build $BUILD_PATH --target doc -- -j$NUM_PROCS
